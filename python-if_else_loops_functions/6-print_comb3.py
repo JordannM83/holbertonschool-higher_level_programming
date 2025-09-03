d1bb6f4256c2fd665c70af4c6583afa792a12340
JordannM83 <11457@holbertonstudents.com>
2025-09-03 10:22:05 +0200

    Add islower and uppercase functions with corresponding test scripts

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+for dizaine in range(9):
+    for unité in range(dizaine + 1, 10):
+        if dizaine == 8 and unité == 9:
+            print(f"{dizaine}{unité}")
+        else:
+            print(f"{dizaine}{unité}, ", end="")
