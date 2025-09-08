b32c5c0aad0497c48268d81448f26d3b8f87e052
JordannM83 <11457@holbertonstudents.com>
2025-09-08 10:42:44 +0200

    Fix formatting in print functions and implement print_matrix_integer

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    pass
+    if matrix == [[]]:
+        print("")
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix[i])):
+            if j < len(matrix[i]) - 1:
+                print('{:d} '.format(matrix[i][j]), end="")
+            else:
+                print('{:d}'.format(matrix[i][j]))
