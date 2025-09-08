0f7e81e3c10dcec8ea91ed3cabb8a1df26b19e2e
JordannM83 <11457@holbertonstudents.com>
2025-09-08 15:28:58 +0200

    Refactor square_matrix_simple to create a new matrix and fix element assignment; Implement search_replace function to replace specified elements in a list

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = list(matrix)
+    new_matrix = [list(line) for line in matrix]
     for i in range(0, len(new_matrix)):
         for j in range(0, len(new_matrix[i])):
-            new_matrix[i][j] *= new_matrix[i][j]
+            new_matrix[i][j] = matrix[i][j] * matrix[i][j]
     return new_matrix
