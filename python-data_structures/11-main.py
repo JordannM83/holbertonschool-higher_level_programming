642f7adddaaa3cb51fe36ea60a00ef3a24f061f9
JordannM83 <11457@holbertonstudents.com>
2025-09-08 14:19:00 +0200

    Implement delete_at function to remove an element at a specified index from a list

--- python-data_structures/11-main.py
+++ python-data_structures/11-main.py
@@ -6,3 +6,21 @@ idx = 3
 new_list = delete_at(my_list, idx)
 print(new_list)
 print(my_list)
+
+my_list = [1, 2, 3, 4, 5]
+idx = 4
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)
+
+my_list = [1, 2, 3, 4, 5]
+idx = 5
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)
+
+my_list = [1, 2, 3, 4, 5]
+idx = -1
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)
