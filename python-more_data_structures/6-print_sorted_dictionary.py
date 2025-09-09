910df5fbdd3cc7c367a00ef0d520aa94eeb92ad3
JordannM83 <11457@holbertonstudents.com>
2025-09-09 10:48:27 +0200

    Implement functions for best score, unique addition, common elements, only different elements, number of keys, sorted dictionary printing, dictionary updating, simple deletion, and multiplication by 2

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    pass
+    for x, y in sorted(a_dictionary.items()):
+        print("{}: {}".format(x, y))
--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    pass
+    if a_dictionary.get(key):
+        a_dictionary.pop(key)
+    return a_dictionary
