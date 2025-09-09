84c2df1e45515cc76e9891aa99d1e5cf769c5f9c
JordannM83 <11457@holbertonstudents.com>
2025-09-09 13:36:48 +0200

    Refactor best_score function for clarity; Implement multiply_list_map function; Fix uniq_add initialization

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    
+    max_score = None
+    winner = None
+    if not a_dictionary:
+        return None
+    for name, score in a_dictionary.items():
+        if max_score is None:
+            max_score = score
+            winner = name
+        if score > max_score:
+            max_score = score
+            winner = name
+    return winner
--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     sorted(my_list)
-    doing = 0
-    result = 0
+    doing = my_list[0]
+    result = my_list[0]
     for i in range(0, len(my_list)):
         if my_list[i] > doing:
             result += my_list[i]
