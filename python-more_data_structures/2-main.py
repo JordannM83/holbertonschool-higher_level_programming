670b654b3d7bfae258a9c34d6cefd93b07e8d818
JordannM83 <11457@holbertonstudents.com>
2025-09-09 13:40:24 +0200

    Add additional test cases for uniq_add function and ensure it handles empty lists

--- python-more_data_structures/2-main.py
+++ python-more_data_structures/2-main.py
@@ -4,3 +4,9 @@ uniq_add = __import__('2-uniq_add').uniq_add
 my_list = [1, 2, 3, 1, 4, 2, 5]
 result = uniq_add(my_list)
 print("Result: {:d}".format(result))
+my_list = [-1, -2, -3, -1, -4, -2, -5]
+result = uniq_add(my_list)
+print("Result: {:d}".format(result))
+my_list = []
+result = uniq_add(my_list)
+print("Result: {:d}".format(result))
--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    if not my_list:
+        return 0
     sorted(my_list)
     doing = my_list[0]
     result = my_list[0]
