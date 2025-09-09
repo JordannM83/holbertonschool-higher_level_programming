db359e0591304977467b7324c9169dc05975205d
JordannM83 <11457@holbertonstudents.com>
2025-09-09 10:49:47 +0200

    Remove debug print statement from uniq_add function

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -3,7 +3,6 @@ def uniq_add(my_list=[]):
     sorted(my_list)
     doing = 0
     result = 0
-    print(my_list)
     for i in range(0, len(my_list)):
         if my_list[i] > doing:
             result += my_list[i]
