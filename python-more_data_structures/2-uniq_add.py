bbc3b256ca49ab3b028207e94ad48fcc25145ccd
JordannM83 <11457@holbertonstudents.com>
2025-09-09 13:57:20 +0200

    Refactor uniq_add function to simplify logic and improve performance by using set for unique values

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if not my_list:
-        return 0
-    sorted(my_list)
-    doing = my_list[0]
-    result = my_list[0]
-    for i in range(0, len(my_list)):
-        if my_list[i] > doing:
-            result += my_list[i]
-            doing += 1
-    return result
+    return sum(set(my_list))
