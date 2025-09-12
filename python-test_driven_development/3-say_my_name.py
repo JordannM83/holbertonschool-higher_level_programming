a873e0cfa90ef1a5c4341afa6e54bd341ffa7c6b
JordannM83 <11457@holbertonstudents.com>
2025-09-12 10:25:47 +0200

    Implement say_my_name function with input validation and error handling

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -14,4 +14,9 @@ def say_my_name(first_name, last_name=""):
     Raises:
         TypeError: if first_name or last_name is not a string
     """
-    pass
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    
+    print("My name is {} {}".format(first_name, last_name))
