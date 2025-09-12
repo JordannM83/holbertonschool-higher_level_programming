ef47fa41d35bbaf09f643c536b47e86b2293f049
JordannM83 <11457@holbertonstudents.com>
2025-09-12 11:21:59 +0200

    Update .gitignore and implement max_integer function with corresponding tests

--- python-test_driven_development/6-max_integer.py
+++ python-test_driven_development/6-max_integer.py
@@ -1,51 +1,18 @@
 #!/usr/bin/python3
-"""
-Module that contains the class Rectangle
+"""Module to find the max integer in a list
 """
 
 
-class Rectangle:
-    """Rectangle class with width and height"""
-    
-    def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
-        
-        Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
-        """
-        pass
-    
-    @property
-    def width(self):
-        """Get/set the width of the Rectangle."""
-        pass
-    
-    @width.setter
-    def width(self, value):
-        pass
-    
-    @property
-    def height(self):
-        """Get/set the height of the Rectangle."""
-        pass
-    
-    @height.setter
-    def height(self, value):
-        pass
-    
-    def area(self):
-        """Return the area of the Rectangle."""
-        pass
-    
-    def perimeter(self):
-        """Return the perimeter of the Rectangle."""
-        pass
-    
-    def __str__(self):
-        """Return the printable representation of the Rectangle."""
-        pass
-    
-    def __repr__(self):
-        """Return the string representation of the Rectangle."""
-        pass
+def max_integer(list=[]):
+    """Function to find and return the max integer in a list of integers
+        If the list is empty, the function returns None
+    """
+    if len(list) == 0:
+        return None
+    result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > result:
+            result = list[i]
+        i += 1
+    return result
