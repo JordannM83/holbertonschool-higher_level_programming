2b1b88bb2fc74a1c3946dda6d82201b14a8a7b71
JordannM83 <11457@holbertonstudents.com>
2025-09-15 15:43:05 +0200

    Implement Square class with size validation, area calculation, and printing functionality

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -7,3 +7,42 @@ to print itself using the '#' character.
 """
 
 
+class Square:
+    """A class that defines a square.
+    This is an empty class that serves as the foundation for building
+    a more complete Square class in future iterations.
+    """
+    __size = None
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if self.size == 0:
+            print("")
+        else:
+            for i in range(0, self.size):
+                for j in range(0, self.size):
+                    print("#", end="")
+                print("")
