e71d643b8c160bd5980ddce5ba3f04bd85cdc302
JordannM83 <11457@holbertonstudents.com>
2025-09-16 09:20:35 +0200

    Fix position setter validation to ensure it checks for a tuple of two positive integers

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -50,7 +50,8 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, int) or value < 0:
+        if (not isinstance(value, tuple) or value[0] < 0
+           or value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
