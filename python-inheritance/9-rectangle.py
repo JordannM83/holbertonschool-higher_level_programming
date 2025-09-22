95303b2987d3567a8193940d6d8f9dae0aa44fb0
JordannM83 <11457@holbertonstudents.com>
2025-09-22 17:51:44 +0200

    Refactor Rectangle class to remove redundant BaseGeometry definition and streamline width and height initialization

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -2,39 +2,7 @@
 """
 This module contains a BaseGeometry class with an area method.
 """
-
-
-class BaseGeometry:
-    """
-    A BaseGeometry class with an area method that raises an Exception.
-
-    This class serves as a base for geometric shapes and operations.
-    """
-
-    def area(self):
-        """
-        Calculates the area of the geometry.
-
-        Raises:
-        Exception: Always raises an Exception with the message
-        "area() is not implemented"
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Public instance method: def integer_validator(self, name, value):
-        that validates value:
-        you can assume name is always a string
-        if value is not an integer: raise a TypeError exception,
-        with the message <name> must be an integer
-        if value is less or equal to 0: raise a ValueError exception with the
-        message <name> must be greater than 0
-        """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        elif value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -46,8 +14,13 @@ class Rectangle(BaseGeometry):
         """
         this module is the initilisation of rectangle
         """
-        if BaseGeometry.integer_validator(self, "width", width) and width > 0:
-            __width = width
-        if (BaseGeometry.integer_validator(self, "height", height) and
-           height > 0):
-            __height = height
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def __str__(self):
+        return f"[{type(self).__name__}] {self.__width}/{self.__height}"
+
+    def area(self):
+        return self.__width * self.__height
