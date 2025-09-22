a08f09769006453a8229e2a2f19aeba0e2f49b18
JordannM83 <11457@holbertonstudents.com>
2025-09-22 11:10:53 +0200

    Fix inherits_from function to correctly check for subclass instances and add test cases for MyList and BaseGeometry modules

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -18,4 +18,4 @@ def inherits_from(obj, a_class):
         bool: True if obj is an instance of a subclass of a_class,
               False otherwise
     """
-    return type(obj) is not a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
