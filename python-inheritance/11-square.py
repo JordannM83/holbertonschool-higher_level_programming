5a22fff042c50a7f69150ec7b3f3fd09e775519e
JordannM83 <11457@holbertonstudents.com>
2025-09-23 12:15:23 +0200

    Implement Square class as a subclass of Rectangle with size validation and string representation

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+create a subclass square from rectangle
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Write a class Square that inherits from Rectangle
+    """
+    def __init__(self, size):
+        """
+        this module is the initilisation of square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        return (f"[{type(self).__name__}] {self._Rectangle__width}/"
+                f"{self._Rectangle__height}")
