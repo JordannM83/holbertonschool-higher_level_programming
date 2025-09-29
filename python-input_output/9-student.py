b7ec54de9b650eca4099f7dcd0ab212fb653f6e0
JordannM83 <11457@holbertonstudents.com>
2025-09-29 18:18:02 +0200

    Fix attribute assignment in Student class constructor to use instance variables

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -18,9 +18,9 @@ class Student:
             last_name (str): The last name of the student.
             age (int): The age of the student.
         """
-        first_name = first_name
-        last_name = last_name
-        age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """
