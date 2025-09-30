a34d2c72df858cece9dad205c6725641f2de96ab
JordannM83 <11457@holbertonstudents.com>
2025-09-30 10:28:15 +0200

    Fix formatting and add missing newlines in serialization modules

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -2,19 +2,22 @@
 """
 Basic serialization module.
 
-This module provides functionality to serialize a Python dictionary to a JSON file
-and deserialize the JSON file to recreate the Python Dictionary.
+This module provides functionality to serialize a
+Python dictionary to a JSON file
+and deserialize the JSON file to recreate
+the Python Dictionary.
 """
 
 
 def serialize_and_save_to_file(data, filename):
     """
     Serialize a Python dictionary to a JSON file.
-    
+
     Args:
         data: A Python Dictionary with data
-        filename: The filename of the output JSON file. If the output file 
-                 already exists it should be replaced.
+        filename: The filename of the output JSON file.
+        If the output file
+        already exists it should be replaced.
     """
     pass
 
@@ -22,10 +25,10 @@ def serialize_and_save_to_file(data, filename):
 def load_and_deserialize(filename):
     """
     Load and deserialize data from a JSON file.
-    
+
     Args:
         filename: The filename of the input JSON file
-        
+
     Returns:
         A Python Dictionary with the deserialized JSON data from the file.
     """
--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -2,7 +2,7 @@
 """
 XML serialization and deserialization module.
 
-This module explores serialization and deserialization using XML as an 
+This module explores serialization and deserialization using XML as an
 alternative format to JSON.
 """
 
@@ -10,7 +10,7 @@ alternative format to JSON.
 def serialize_to_xml(dictionary, filename):
     """
     Serialize a Python dictionary into XML and save it to a file.
-    
+
     Args:
         dictionary (dict): The Python dictionary to serialize
         filename (str): The filename to save the XML data to
@@ -21,10 +21,10 @@ def serialize_to_xml(dictionary, filename):
 def deserialize_from_xml(filename):
     """
     Read XML data from a file and return a deserialized Python dictionary.
-    
+
     Args:
         filename (str): The filename to read XML data from
-        
+
     Returns:
         dict: The deserialized Python dictionary
     """
