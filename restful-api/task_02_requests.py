8937cdd37e8b7c81b260bf012972425daa4b93bb
JordannM83 <11457@holbertonstudents.com>
2025-10-06 16:53:59 +0200

    Update .gitignore to include .venv and modify fetch_and_print_posts to correctly fetch posts from JSONPlaceholder

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -5,9 +5,10 @@ import csv
 
 def fetch_and_print_posts():
     """Fetch all posts from JSONPlaceholder and print titles"""
-    r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
-    print (r.status_code)
-
+    r = requests.get('https://jsonplaceholder.typicode.com/posts')
+    print (f"Status Code: {r.status_code}")
+    if r.status_code == 200:
+        print (r.json())
 
 def fetch_and_save_posts():
     """Fetch all posts from JSONPlaceholder and save to CSV"""
