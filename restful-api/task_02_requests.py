793c91f5bcab6324a2349e7caf0154b89287bb65
JordannM83 <11457@holbertonstudents.com>
2025-10-07 09:42:16 +0200

    Fetch posts from JSONPlaceholder and save to CSV; improve output formatting

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -6,10 +6,20 @@ import csv
 def fetch_and_print_posts():
     """Fetch all posts from JSONPlaceholder and print titles"""
     r = requests.get('https://jsonplaceholder.typicode.com/posts')
-    print (f"Status Code: {r.status_code}")
+    print(f"Status Code: {r.status_code}")
     if r.status_code == 200:
-        print (r.json())
+        r = r.json()
+        for dictionary in r:
+            print(dictionary['title'])
+
 
 def fetch_and_save_posts():
     """Fetch all posts from JSONPlaceholder and save to CSV"""
-    pass
+    r = requests.get('https://jsonplaceholder.typicode.com/posts')
+    if r.status_code == 200:
+        with open('posts.csv', 'w', newline='') as csvfile:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
+                                    extrasaction='ignore')
+            writer.writeheader()
+            writer.writerows(r.json())
