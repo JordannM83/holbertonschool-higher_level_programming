f4dd1c6d09cbe9a1ae9c38b83af3abf9fe33096e
JordannM83 <11457@holbertonstudents.com>
2025-10-07 14:54:26 +0200

    Implement GET request handling for a simple HTTP server with multiple endpoints

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -9,15 +9,45 @@ import json
 
 class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     """Simple HTTP request handler"""
-    
+
     def do_GET(self):
         """Handle GET requests"""
-        pass
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            dic1 = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(dic1).encode())
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"ok")
+        elif self.path == '/info':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            dic2 = {"version": "1.0", "description":
+                    "A simple API built with http.server"}
+            self.wfile.write(json.dumps(dic2).encode())
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Error Endpoint not found")
 
 
-def run(server_class=http.server.HTTPServer, handler_class=SimpleHTTPRequestHandler):
-    """Run the HTTP server"""
-    pass
+def run(server_class=http.server.HTTPServer,
+        handler_class=SimpleHTTPRequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    print("Serveur en cours d'exécution sur http://localhost:8000")
+    httpd.serve_forever()
 
 
 if __name__ == "__main__":
