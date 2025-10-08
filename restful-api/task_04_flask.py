afcb7575c83c792fb152a42d6c7244354ad038f5
JordannM83 <11457@holbertonstudents.com>
2025-10-08 15:02:57 +0200

    Implement functionality for home, data, status, user retrieval, and user addition endpoints in Flask API

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -13,31 +13,41 @@ users = {}
 @app.route('/')
 def home():
     """Home endpoint"""
-    pass
+    return "Welcome to the Flask API!"
 
 
 @app.route('/data')
 def get_data():
     """Data endpoint - returns list of usernames"""
-    pass
+    return jsonify(list(users.keys()))
 
 
 @app.route('/status')
 def get_status():
     """Status endpoint"""
-    pass
+    return "OK"
 
 
 @app.route('/users/<username>')
 def get_user(username):
     """Get user by username"""
-    pass
+    try:
+        return jsonify(users[username])
+    except KeyError:
+        return jsonify({"error": "User not found"})
 
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
     """Add a new user"""
-    pass
+    data = request.get_json()
+    if "username" not in data:
+        return jsonify({"error": "Username is required"}), 400
+    users[data["username"]] = data
+    return jsonify({
+                    "message": "User added",
+                    "user": data
+                    }), 201
 
 
 if __name__ == '__main__':
