2c293db8755686d2262472efdfb7476221481483
JordannM83 <11457@holbertonstudents.com>
2025-10-08 15:17:17 +0200

    Refactor add_user endpoint to simplify error handling and remove unnecessary try-except block

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -40,18 +40,15 @@ def get_user(username):
 @app.route('/add_user', methods=['POST'])
 def add_user():
     """Add a new user"""
-    try:
-        data = request.get_json()
-        if not data or "username" not in data or not data.get("username"):
-            return jsonify({"error": "Username is required"}), 400
-        users[data["username"]] = data
-        return jsonify({
-                        "message": "User added",
-                        "user": data
-                        }), 201
-    except Exception:
+    data = request.get_json()
+    if not data or "username" not in data:
         return jsonify({"error": "Username is required"}), 400
+    users[data["username"]] = data
+    return jsonify({
+        "message": "User added",
+        "user": data
+    }), 201
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=False, host='127.0.0.1', port=5000)
