8f3f9d4aa5df3e10e1b865bad616501e42716ea0
JordannM83 <11457@holbertonstudents.com>
2025-10-21 10:35:16 +0200

    Add SQL scripts for user management, database creation, and show queries

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost)
+-- This script shows all grants for user_0d_1 and user_0d_2
+SHOW GRANTS FOR user_0d_1;
+SHOW GRANTS FOR user_0d_2;
