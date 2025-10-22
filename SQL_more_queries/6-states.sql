d2237ff67c0713e41cc6d321f3fd92515469bd2b
JordannM83 <11457@holbertonstudents.com>
2025-10-22 11:59:50 +0200

    Fix SQL scripts to ensure proper database usage and add missing SELECT statement in cities_by_state_join.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -5,6 +5,7 @@
 -- If the database hbtn_0d_usa already exists, your script should not fail
 -- If the table states already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
     id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
     name VARCHAR(256) NOT NULL
