31a66485fedfd7255c0b93426d1eb70393898f7c
JordannM83 <11457@holbertonstudents.com>
2025-10-23 10:57:32 +0200

    Update .gitignore to include venv and add initial state selection script

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,3 +2,29 @@
 """
 Script that lists all states from the database hbtn_0e_0_usa
 """
+import MySQLdb
+import sys
+
+
+def main():
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all results
+    results = cursor.fetchall()
+
+    # Print each row
+    for row in results:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
