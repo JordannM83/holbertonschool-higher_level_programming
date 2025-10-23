cd7a5834266d2baf2c8f6c1718ca47690c91f6bf
JordannM83 <11457@holbertonstudents.com>
2025-10-23 20:36:06 +0200

    Add port specification to database connection in 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -9,6 +9,7 @@ import sys
 
 def main():
     db = MySQLdb.connect(host="localhost",
+                         port=3306,
                          user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3])
