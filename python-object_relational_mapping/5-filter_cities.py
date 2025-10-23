48343f3b86557efa3896049ea52b5b51a3962248
JordannM83 <11457@holbertonstudents.com>
2025-10-23 20:31:55 +0200

    Refactor SQL queries in filter_states.py and filter_cities.py for improved readability and consistency

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -13,7 +13,9 @@ def main():
                          passwd=sys.argv[2],
                          db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = %s) ORDER BY cities.id", (sys.argv[4],))
+    cursor.execute("SELECT name FROM cities WHERE state_id = (SELECT id "
+                   "FROM states WHERE name = %s) "
+                   "ORDER BY cities.id", (sys.argv[4],))
     results = cursor.fetchall()
 
     cities = [row[0] for row in results]
