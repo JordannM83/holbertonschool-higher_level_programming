e09d8b876298a681398538ddfdc24c3cc67eaeb9
JordannM83 <11457@holbertonstudents.com>
2025-10-23 20:27:15 +0200

    Add scripts for state and city management with database queries

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -2,3 +2,26 @@
 """
 Script that lists all cities from the database hbtn_0e_4_usa
 """
+import MySQLdb
+import sys
+
+
+def main():
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities "
+                   "JOIN states ON cities.state_id = states.id")
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -3,3 +3,25 @@
 Script that takes in the name of a state as an argument
 and lists all cities of that state
 """
+import MySQLdb
+import sys
+
+
+def main():
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = %s) ORDER BY cities.id", (sys.argv[4],))
+    results = cursor.fetchall()
+
+    cities = [row[0] for row in results]
+    print(", ".join(cities))
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
