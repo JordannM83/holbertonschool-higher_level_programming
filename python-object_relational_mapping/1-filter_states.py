457e40de3c136103693a5ace27dacf41bd409faf
JordannM83 <11457@holbertonstudents.com>
2025-10-23 15:12:58 +0200

    Refactor 1-filter_states.py to improve code structure and readability

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -3,3 +3,25 @@
 Script that lists all states with a name starting with N
 from the database hbtn_0e_0_usa
 """
+import MySQLdb
+import sys
+
+
+def main():
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'")
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
