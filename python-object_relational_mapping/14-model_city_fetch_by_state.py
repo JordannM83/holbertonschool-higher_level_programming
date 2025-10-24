3bdbf7a0d04fab3111ed0f5bb3f3bd3bbc5ab1c4
JordannM83 <11457@holbertonstudents.com>
2025-10-24 16:46:31 +0200

    Refactor City class definition and improve database query for City objects

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -2,3 +2,24 @@
 """
 Script that prints all City objects from the database hbtn_0e_14_usa
 """
+from model_state import Base, State
+from model_city import City
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    results = session.query(City, State).join(State).order_by(City.id).all()
+    for city, state in results:
+        print(f"{state.name}: ({city.id}) {city.name}")
+
+    session.close()
