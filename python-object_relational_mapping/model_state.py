9772fe0d4caf6339f565fea8b7240ad0981295a1
JordannM83 <11457@holbertonstudents.com>
2025-10-24 10:41:43 +0200

    Add initial implementation of State class and database connection setup

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,3 +2,16 @@
 """
 Module that contains the class definition of a State
 """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    The class definition of a State
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
