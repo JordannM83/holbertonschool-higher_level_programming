e1dc563bb515feeb8822f6b407ee2cdb529f4de8
JordannM83 <11457@holbertonstudents.com>
2025-11-07 11:38:49 +0100

    Implement square printing based on user-defined size input

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const { argv } = require('node:process');
+const size = parseInt(argv[2]);
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 1; i <= size; i++) {
+    let line = '';
+    for (let j = 1; j <= size; j++) {
+      line += 'X';
+    }
+    console.log(line);
+  }
+}
