f691ab60cb33d20fde618918a61a90f0b3cd828c
JordannM83 <11457@holbertonstudents.com>
2025-11-07 10:45:21 +0100

    Simplify argument logging to display only the first argument in 3-value_argument.js

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -3,8 +3,5 @@ const { argv } = require('node:process');
 if (argv[2] == null) {
   console.log('No argument');
 } else {
-  const args = process.argv.slice(2);
-  args.forEach(value => {
-    console.log(value);
-  });
+  console.log(argv[2]);
 }
