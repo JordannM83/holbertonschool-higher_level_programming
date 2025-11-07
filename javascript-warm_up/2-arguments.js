8127542d0334480164ce5c5e4c715c3319010d81
JordannM83 <11457@holbertonstudents.com>
2025-11-07 10:41:06 +0100

    Fix argument detection logic to prioritize multiple arguments in 2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-if (argv[2] != null) {
+if (argv[3] != null) {
+  console.log('Arguments found');
+} else if (argv[2] != null) {
   console.log('Argument found');
 } else {
   console.log('No argument');
