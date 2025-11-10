af8a6045799bbe5ade9029ed3b457338f9716aba
JordannM83 <11457@holbertonstudents.com>
2025-11-10 15:23:52 +0100

    Add movie and translation fetching functionality to scripts

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -1,1 +1,15 @@
 // JavaScript script that fetches and lists the title for all movies from https://swapi-api.hbtn.io/api/films/?format=json
+const listMoviesElement = document.getElementById('list_movies');
+
+fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    data.results.forEach(movie => {
+      const li = document.createElement('li');
+      li.textContent = movie.title;
+      listMoviesElement.appendChild(li);
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching movies:', error);
+  });
--- javascript-dom_manipulation/8-script.js
+++ javascript-dom_manipulation/8-script.js
@@ -1,1 +1,13 @@
 // JavaScript script that fetches from https://hellosalut.stefanbohacek.com/?lang=fr and displays the value of hello
+document.addEventListener('DOMContentLoaded', function () {
+  const helloElement = document.getElementById('hello');
+
+  fetch('https://hellosalut.stefanbohacek.com/?lang=fr')
+    .then(response => response.json())
+    .then(data => {
+      helloElement.textContent = data.hello;
+    })
+    .catch(error => {
+      console.error('Error fetching translation:', error);
+    });
+});
